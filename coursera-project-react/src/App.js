c8265f045e443a92946a356713b1d2deb3ef803a
SebastianLabadie <sebastianlabadie.contact@gmail.com>
2020-07-31 16:21:04 -0300

    Components Part 1.

--- coursera-project-react/src/App.js
+++ coursera-project-react/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand , Button} from 'reactstrap';
+import Menu from './components/MenuComponent'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Navbar dark color="primary" fixed ="top">
+     <Navbar dark color="primary" >
        <div className="container">
           <NavbarBrand href="/" color="red"> Ristorante Con Fusion</NavbarBrand>
           <Button variant="secondary">ASD</Button>
          </div>
     </Navbar>
+    <Menu/>
     </div>
   );
 }
